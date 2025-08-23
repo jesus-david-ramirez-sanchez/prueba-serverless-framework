3a0dc47930c8ad99aff4531ae77ef72f97edfad5
Jesus David Ramirez <jesusdavidramirezsanchez@hotmail.com>
2025-08-23 15:20:28 -0500

    Agregar archivos iniciales para la API de biblioteca: .gitignore, package.json, serverless.yml y función de manejador en TypeScript.

---
+++ library-shop-api/src/functions/halndler.ts
@@ -0,0 +1,10 @@
+// src/functions/handler.ts
+export const handler = async (event: any) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Go Serverless v3.0! Your function executed successfully!',
+            input: event,
+        }),
+    };
+};
